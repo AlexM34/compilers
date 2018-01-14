aec0a7782b4511cd5fcb3aa9afd2061956b02969
Aleksandar Monev <ug15am@msc26.tcs.cs.ox.ac.uk>
2018-01-14 12:38:40 +0000

    First commit

---
+++ b.s
@@ -0,0 +1,17 @@
+@ picoPascal compiler output
+	.include "fixup.s"
+	.global pmain
+
+	.text
+pmain:
+	mov ip, sp
+	stmfd sp!, {r4-r10, fp, ip, lr}
+	mov fp, sp
+@   print_num(2); newline()
+	mov r0, #2
+	bl print_num
+	bl newline
+	ldmfd fp, {r4-r10, fp, sp, pc}
+	.ltorg
+
+@ End
